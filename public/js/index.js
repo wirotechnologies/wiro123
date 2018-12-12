45201342a8e5a985978428463710934c6d815487
yeisonking0214 <yeison.mancilla.0214@gmail.com>
2018-12-12 15:54:49 -0500

    React Js Onclick

--- public/js/index.js
+++ public/js/index.js
@@ -5,4 +5,4 @@ import Media from './playlist/components/media.js';
 const app = document.getElementById('app');
 //ReactDOM.render(que voy a renderizar, donde lo haré);
 //const holaMundo = <h1>Hola Mundo!</h1>;
-ReactDOM.render(<Media title="¿Que es responsive Design?" author="Yeison" image="/img/images/bitcoin_.png"/>, app);
+ReactDOM.render(<Media type="video" title="¿Que es responsive Design?" author="Yeison" image="/img/images/bitcoin_.png"/>, app);
--- public/js/playlist/components/media.js
+++ public/js/playlist/components/media.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './../../../css/playlist/components/media.css';
 
 class Media extends Component {
-	render() {
-		return (
-			<div className="Media">
-				<div className="Media-cover">
-					<img
-						src={this.props.image}
-						alt=""
-						width={260}
-						height={160}
-                                                className="Media-imge"
-					/>
-					<h3 className="Media-title">{this.props.title}</h3>
-					<p className="Media-author">{this.props.author}</p>
-				</div>
-			</div>
-		)
-	}	
+    constructor(props) {
+        super(props)
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(event) {
+        console.log(this.props.title);
+    }
+    render() {
+        return (
+            <div className="Media" onClick={this.handleClick}>
+                <div className="Media-cover">
+                    <img
+                            src={this.props.image}
+                            alt=""
+                            width={260}
+                            height={160}
+                            className="Media-imge"
+                    />
+                    <h3 className="Media-title">{this.props.title}</h3>
+                    <p className="Media-author">{this.props.author}</p>
+                </div>
+            </div>
+        )
+    }	
+}
+
+Media.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string,
+  type: PropTypes.oneOf(['video', 'audio']),
 }
 
 export default Media;
